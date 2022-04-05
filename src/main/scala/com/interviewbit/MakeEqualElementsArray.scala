c9c1d5e57546517f49d4ed9cc5467cf29e18e20c
Nikolett Harsanyi <nikolett.harsanyi@trinitymirror.com>
2022-04-05 20:44:06 +0100

    RepeatedNumber

--- src/main/scala/com/interviewbit/MakeEqualElementsArray.scala
+++ src/main/scala/com/interviewbit/MakeEqualElementsArray.scala
@@ -4,6 +4,7 @@ object MakeEqualElementsArray {
 
   def solve(A: Array[Int], B: Int): Int  = {
     val a_minus_x = A(0) - B
+    A.length
     val a = A(0)
     val a_plus_x = A(0) + B
     val n = A.length
