969287157e3c5040a5c0708965934726b36d53ba
nikike <nikike@nikoletts-MacBook-Pro.local>
2022-05-10 19:33:21 +0100

    TwoSum

---
+++ src/main/scala/com/leetcode/TwoSum.scala
@@ -0,0 +1,31 @@
+package com.leetcode
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object TwoSum {
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val hashMap = mutable.HashMap.empty[Int, ListBuffer[Int]]
+    val n = nums.length
+    for (i <- 0 to n - 1) {
+      val num = nums(i)
+      val oldIndex = hashMap.getOrElse(num, ListBuffer.empty).addOne(i)
+      hashMap += num -> oldIndex
+    }
+    var i = 0
+    for (k <- nums) {
+      val diff = target - k
+      if (diff == k && (hashMap.contains(diff) && hashMap.get(diff).get.length > 1)) {
+        val indeces = hashMap.get(diff).get.sorted
+        return Array(indeces(0), indeces(1))
+      } else if (diff != k && hashMap.contains(diff)) {
+        val diffIndex = hashMap.get(diff).get.head
+        return Array(i, diffIndex)
+      }
+      i += 1
+    }
+    Array() // it should never happen, since we have result garantued
+  }
+
+}
---
+++ src/test/scala/com/leetcode/TwoSumTest.scala
@@ -0,0 +1,24 @@
+package com.leetcode
+
+import org.scalatest.FunSuite
+
+class TwoSumTest extends FunSuite {
+
+  test("should return result for input 1") {
+    val input = Array(2,7,11,15)
+    val target = 9
+    assertResult(Array(0, 1))(TwoSum.twoSum(input, target))
+  }
+
+  test("should return result for input 2") {
+    val input = Array(3, 3)
+    val target = 6
+    assertResult(Array(0, 1))(TwoSum.twoSum(input, target))
+  }
+
+  test("should return result for input 3") {
+    val input = Array(3, 2, 4)
+    val target = 6
+    assertResult(Array(1, 2))(TwoSum.twoSum(input, target))
+  }
+}
